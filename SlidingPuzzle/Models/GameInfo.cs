2157c13d68bb2b391296204cee73aa3bf4d51a4b
Mlendea Horatiu <Mlendea.Horatiu@GMail.com>
2016-06-16 20:22:58 +0300

    Created the models

---
+++ SlidingPuzzle/Models/GameInfo.cs
@@ -0,0 +1,35 @@
+﻿namespace SlidingPuzzle.Models
+{
+    public class GameInfo
+    {
+        /// <summary>
+        /// Gets or sets the game time.
+        /// </summary>
+        /// <value>The game time.</value>
+        public int GameTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the size of the table.
+        /// </summary>
+        /// <value>The size of the table.</value>
+        public int TableSize { get; set; }
+
+        /// <summary>
+        /// Gets or sets the moves.
+        /// </summary>
+        /// <value>The moves.</value>
+        public int Moves { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is running.
+        /// </summary>
+        /// <value><c>true</c> if this instance is running; otherwise, <c>false</c>.</value>
+        public bool IsRunning { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="SlidingPuzzle.Models.GameInfo"/> is completed.
+        /// </summary>
+        /// <value><c>true</c> if completed; otherwise, <c>false</c>.</value>
+        public bool Completed { get; set; }
+    }
+}
---
+++ SlidingPuzzle/Models/Tile.cs
@@ -0,0 +1,12 @@
+﻿namespace SlidingPuzzle.Models
+{
+    public class Tile : EntityBase
+    {
+        /// <summary>
+        /// Gets or sets the number.
+        /// </summary>
+        /// <value>The number.</value>
+        public int Number { get; set; }
+    }
+}
+
