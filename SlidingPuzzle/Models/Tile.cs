de2de0230dca07db4bddf85e5c99ea4f62d664c5
Mlendea Horatiu <Mlendea.Horatiu@GMail.com>
2016-06-16 21:43:00 +0300

    Revamped the models and the repository

--- SlidingPuzzle/Models/Tile.cs
+++ SlidingPuzzle/Models/Tile.cs
@@ -1,29 +1,61 @@
-﻿namespace SlidingPuzzle.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SlidingPuzzle.Models
 {
-    public class Tile : EntityBase
+    public class Tile
     {
-        /// <summary>
-        /// Gets or sets the identifier.
-        /// </summary>
-        /// <value>The identifier.</value>
-        public override string Id { get { return X + "," + Y; } }
-
-        /// <summary>
-        /// Gets or sets the number.
-        /// </summary>
-        /// <value>The number.</value>
-        public int Number { get; set; }
-
         /// <summary>
         /// Gets or sets the X coordinate.
         /// </summary>
         /// <value>The X coordinate.</value>
+        [Key]
+        [Range(0, int.MaxValue)]
         public int X { get; set; }
 
         /// <summary>
         /// Gets or sets the Y coordinate.
         /// </summary>
         /// <value>The Y coordinate.</value>
+        [Key]
+        [Range(0, int.MaxValue)]
         public int Y { get; set; }
+
+        /// <summary>
+        /// Gets or sets the number.
+        /// </summary>
+        /// <value>The number.</value>
+        [Range(0, int.MaxValue)]
+        public int Number { get; set; }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object"/> is equal to the current <see cref="SlidingPuzzle.Models.Tile"/>.
+        /// </summary>
+        /// <param name="obj">The <see cref="System.Object"/> to compare with the current <see cref="SlidingPuzzle.Models.Tile"/>.</param>
+        /// <returns><c>true</c> if the specified <see cref="System.Object"/> is equal to the current
+        /// <see cref="SlidingPuzzle.Models.Tile"/>; otherwise, <c>false</c>.</returns>
+        public override bool Equals(object obj)
+        {
+            Tile other = obj as Tile;
+            return X == other?.X && Y == other.Y;
+        }
+
+        /// <summary>
+        /// Serves as a hash function for a <see cref="SlidingPuzzle.Models.Tile"/> object.
+        /// </summary>
+        /// <returns>A hash code for this instance that is suitable for use in hashing algorithms and data structures such as a
+        /// hash table.</returns>
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        /// <summary>
+        /// Returns a <see cref="System.String"/> that represents the current <see cref="SlidingPuzzle.Models.Tile"/>.
+        /// </summary>
+        /// <returns>A <see cref="System.String"/> that represents the current <see cref="SlidingPuzzle.Models.Tile"/>.</returns>
+        public override string ToString()
+        {
+            return string.Format("{0} #{1}", base.ToString(), Id);
+        }
     }
 }
---
+++ SlidingPuzzle/Repositories/TileRepository.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections.Generic;
+
+using SlidingPuzzle.Models;
+
+namespace SlidingPuzzle.Repositories
+{
+    /// <summary>
+    /// Repository.
+    /// </summary>
+    public class TileRepository
+    {
+        /// <summary>
+        /// Gets or sets the entities.
+        /// </summary>
+        /// <value>The entities.</value>
+        protected List<Tile> Tiles { get; set; }
+
+        /// <summary>
+        /// Gets the size.
+        /// </summary>
+        /// <value>The size.</value>
+        public int Size
+        {
+            get { return Tiles.Count; }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SlidingPuzzle.Repositories.TileRepository"/> class.
+        /// </summary>
+        public TileRepository()
+        {
+            Tiles = new List<Tile>();
+        }
+
+        /// <summary>
+        /// Add the specified tile.
+        /// </summary>
+        /// <param name="tile">Tile.</param>
+        public void Add(Tile tile)
+        {
+            if (Tiles.Contains(tile))
+                throw new RepositoryException("The specified tile already exists");
+
+            Tiles.Add(tile);
+        }
+
+        /// <summary>
+        /// Get a tile by the specified x and y.
+        /// </summary>
+        /// <param name="x">The x coordinate.</param>
+        /// <param name="y">The y coordinate.</param>
+        public Tile Get(int x, int y)
+        {
+            Tile tile = Tiles.Find(E => E.X == x && E.Y == y);
+
+            if (tile == null)
+                throw new RepositoryException("A tile with the specified coordinates does not exist");
+
+            return tile;
+        }
+
+        /// <summary>
+        /// Get a tile by the specified number.
+        /// </summary>
+        /// <param name="number">Number.</param>
+        public Tile Get(int number)
+        {
+            Tile tile = Tiles.Find(E => E.Number = number);
+
+            if (tile == null)
+                throw new RepositoryException("A tile with the specified coordinates does not exist");
+
+            return tile;
+        }
+
+        /// <summary>
+        /// Gets all tiles.
+        /// </summary>
+        /// <returns>The tiles.</returns>
+        public List<Tile> GetAll()
+        {
+            return Tiles;
+        }
+
+        /// <summary>
+        /// Update the specified tile.
+        /// </summary>
+        /// <param name="tile">Tile.</param>
+        public void Update(Tile tile)
+        {
+            Tile oldTile = Get(tile.X, tile.Y);
+            oldTile.Number = tile.Number;
+        }
+
+        /// <summary>
+        /// Remove the specified tile.
+        /// </summary>
+        /// <param name="tile">Tile.</param>
+        public void Remove(Tile tile)
+        {
+            if (!Contains(tile))
+                throw new RepositoryException("The specified tile does not exist");
+
+            Tiles.Remove(tile);
+        }
+
+        /// <summary>
+        /// Remove the specified x and y.
+        /// </summary>
+        /// <param name="x">The x coordinate.</param>
+        /// <param name="y">The y coordinate.</param>
+        public void Remove(int x, int y)
+        {
+            if (!Contains(x, y))
+                throw new RepositoryException("A tile with the specified coordinates does not exist");
+
+            Tiles.RemoveAll(T => T.X == x && T.Y == y);
+        }
+
+        /// <summary>
+        /// Clear this instance.
+        /// </summary>
+        public void Clear()
+        {
+            Tiles.Clear();
+        }
+
+        /// <summary>
+        /// Contains the specified tile.
+        /// </summary>
+        /// <param name="tile">Tile.</param>
+        public bool Contains(Tile tile)
+        {
+            return Tiles.Find(E => E.Equals(tile)) != null;
+        }
+
+        /// <summary>
+        /// Contains the specified x and y.
+        /// </summary>
+        /// <param name="x">The x coordinate.</param>
+        /// <param name="y">The y coordinate.</param>
+        public bool Contains(int x, int y)
+        {
+            return Tiles.Find(E => E.X == x && E.Y == y) != null;
+        }
+
+        /// <summary>
+        /// Contains the specified number.
+        /// </summary>
+        /// <param name="number">Number.</param>
+        public bool Contains(int number)
+        {
+            return Tiles.Find(E => E.Number == number) != null;
+        }
+    }
+}
