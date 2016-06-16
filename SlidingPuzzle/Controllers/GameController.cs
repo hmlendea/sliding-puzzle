1945a5f66c8f188de17b3caca8ba8861d9bd0da1
Mlendea Horatiu <Mlendea.Horatiu@GMail.com>
2016-06-16 20:31:37 +0300

    Added X and Y coordinate properties to Tile

---
+++ SlidingPuzzle/Controllers/GameController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SlidingPuzzle.Controllers
+{
+    public class GameController
+    {
+        public GameController()
+        {
+        }
+    }
+}
+
--- SlidingPuzzle/Models/Tile.cs
+++ SlidingPuzzle/Models/Tile.cs
@@ -3,10 +3,27 @@
     public class Tile : EntityBase
     {
         /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>The identifier.</value>
+        public override string Id { get { return X + "," + Y; } }
+
+        /// <summary>
         /// Gets or sets the number.
         /// </summary>
         /// <value>The number.</value>
         public int Number { get; set; }
+
+        /// <summary>
+        /// Gets or sets the X coordinate.
+        /// </summary>
+        /// <value>The X coordinate.</value>
+        public int X { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Y coordinate.
+        /// </summary>
+        /// <value>The Y coordinate.</value>
+        public int Y { get; set; }
     }
 }
-
